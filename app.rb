3f72f6cb3eecd3b271bfd7be5076e48fbd33a672
Igor Drozdov <igor@drozdov.net>
2020-12-22 07:09:39 +0500

    assets are set up

--- app.rb
+++ app.rb
@@ -2,9 +2,11 @@ require 'roda'
 
 class Neo < Roda
 
-  plugin :render
+  plugin :render, escape: true
+  plugin :assets, css: 'app.scss', css_opts: {style: :compressed, cache: false}, js: %w[htmx.js ext/include-vals.js], timestamp_paths: true
 
   route do |r|
+    r.assets
     view :home
   end
 end
